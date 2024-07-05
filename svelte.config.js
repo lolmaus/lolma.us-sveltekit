8a3e502d79ad1712cd677b6f67a0411f35105243
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2024-07-05 15:39:39 +0300

    Ignore HTTP errors on prerender Vercel does not server during build, so client-side requests fail. But we don't need them?

--- svelte.config.js
+++ svelte.config.js
@@ -9,13 +9,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		// adapter-static should auto-configure for Vercel when no options are passed here
 		adapter: adapter(),
 
 		prerender: {
-			entries: crawlMarkdownEntries()
+			entries: crawlMarkdownEntries(),
+			handleHttpError: 'ignore'
 		}
 	}
 };
