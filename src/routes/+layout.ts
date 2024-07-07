394aa2320257af0732aba0c98dcde352a5ff906c
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2024-07-07 11:52:30 +0000

    Add JS redirect from root page to lang

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,9 +1,2 @@
-import { redirect } from '@sveltejs/kit';
-
 export const prerender = true;
 
-export async function load({ url }) {
-  if(url.pathname === '/') {
-    return redirect(301, '/en/');
-  }
-}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  // @ts-expect-error Intentionally checking for non-spec userLanguage
+  const langRaw = navigator.language || navigator.userLanguage || 'en-US';
+  const lang = langRaw.slice(0, 2);
+
+  window.location.replace(`/${lang}/`);
+</script>
