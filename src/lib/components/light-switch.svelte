83b22c1c1e8cee69cf844baf72099624466a155a
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2024-07-24 19:37:39 +0000

    Proper dark mode switch

---
+++ src/lib/components/light-switch.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	// Borrowed from Skeleton v2 LightSwitch component.
+	// See the Custom Component section here:
+	// https://www.skeleton.dev/docs/dark-mode
+
+	import { onMount } from 'svelte';
+	import {
+		modeCurrent,
+		setModeUserPrefers,
+		setModeCurrent,
+		setInitialClassState,
+		getModeOsPrefers,
+		type SvelteEvent,
+	} from '@skeletonlabs/skeleton';
+
+	import { Sun, Moon } from 'lucide-svelte';
+
+	// Props
+	/** Customize the `title` attribute for the component.  */
+	export let title = 'Toggle light or dark mode.';
+
+	function onToggleHandler(): void {
+		$modeCurrent = !$modeCurrent;
+		setModeUserPrefers($modeCurrent);
+		setModeCurrent($modeCurrent);
+	}
+
+	// A11y Input Handlers
+	function onKeyDown(event: SvelteEvent<KeyboardEvent, HTMLDivElement>): void {
+		// Enter/Space triggers selection event
+		if (['Enter', 'Space'].includes(event.code)) {
+			event.preventDefault();
+			event.currentTarget.click();
+		}
+	}
+
+	// Lifecycle
+	onMount(() => {
+		// Sync lightswitch with the theme
+		if (!('modeCurrent' in localStorage)) {
+			setModeCurrent(getModeOsPrefers());
+		}
+	});
+</script>
+
+<svelte:head>
+	<!-- Workaround for a svelte parsing error: https://github.com/sveltejs/eslint-plugin-svelte/issues/492 -->
+	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+	{@html `<\u{73}cript nonce="%sveltekit.nonce%">(${setInitialClassState.toString()})();</script>`}
+</svelte:head>
+
+<div
+	class="cursor-pointer"
+	onclick={onToggleHandler}
+	onkeydown={onKeyDown}
+	role="switch"
+	aria-label="Light Switch"
+	aria-checked={$modeCurrent}
+	{title}
+	tabindex="0"
+>
+	{#if $modeCurrent}
+		<Moon size={50} />
+	{:else}
+		<Sun size={50} />
+	{/if}
+</div>
